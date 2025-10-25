549d3667581eb0ad114a230666513a93e8c81774
janrzepiszczak <jan.rzepiszczak@unoprogramo.eu>
2025-10-25 14:33:07 +0200

    Add proto sub repository

--- crates/server/build.rs
+++ crates/server/build.rs
@@ -2,6 +2,6 @@ use tonic_prost_build::configure;
 
 fn main() {
     configure()
-        .compile_protos(&["proto/discovery.proto"], &["proto"])
+        .compile_protos(&["proto/proto/v1/discovery.proto"], &["proto"])
         .unwrap();
 }
